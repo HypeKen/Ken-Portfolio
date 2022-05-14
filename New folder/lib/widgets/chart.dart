daa4f3c7a56af05c5bc4af49a5007758d8fefe2f
HypeKen <ken@areahype.com>
2022-05-13 22:48:21 -0500

    Update

---
+++ New folder/lib/widgets/chart.dart
@@ -0,0 +1,89 @@
+// ignore_for_file: always_specify_types, avoid_as
+
+import 'package:expensecalculator/index.dart';
+import 'package:intl/intl.dart';
+
+class Chart extends StatelessWidget {
+  const Chart({
+    required this.recentTransactions,
+    final Key? key,
+  }) : super(key: key);
+
+  final List<Transaction> recentTransactions;
+
+  List<Map<String, Object>> get groupedTransactionValues {
+    return List.generate(7, (final int index) {
+      final DateTime weekDay = DateTime.now().subtract(
+        Duration(days: index),
+      );
+
+      var totalSum = 0.0;
+
+      for (var i = 0; i < recentTransactions.length; i++) {
+        if (recentTransactions[i].date.day == weekDay.day &&
+            recentTransactions[i].date.month == weekDay.month &&
+            recentTransactions[i].date.year == weekDay.year) {
+          totalSum += recentTransactions[i].amount;
+        }
+      }
+
+      return {
+        'day': DateFormat.E().format(weekDay).substring(0, 3),
+        'amount': totalSum
+      };
+    });
+  }
+
+  double get totalSpending {
+    return groupedTransactionValues.fold(0, (final sum, final item) {
+      // ignore: cast_nullable_to_non_nullable
+      return sum + (item['amount'] as double);
+    });
+  }
+
+  @override
+  Widget build(final BuildContext context) {
+    return Card(
+      elevation: 6,
+      margin: const EdgeInsets.all(20),
+      child: SizedBox(
+        height: 300,
+        width: 300,
+        child: Row(
+          children:
+              groupedTransactionValues.map((final Map<String, Object> data) {
+            return ChartBar(
+              // ignore: cast_nullable_to_non_nullable
+              label: data['day'] as String,
+              // ignore: cast_nullable_to_non_nullable
+              spendingAmount: data['amount'] as double,
+              spendingPercentage: totalSpending == 0
+                  ? 0
+                  // ignore: cast_nullable_to_non_nullable
+                  : (data['amount'] as double) / totalSpending,
+            );
+          }).toList(),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void debugFillProperties(final DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties
+      ..add(
+        IterableProperty<Map<String, Object>>(
+          'groupedTransactionValues',
+          groupedTransactionValues,
+        ),
+      )
+      ..add(
+        IterableProperty<Transaction>(
+          'recentTransactions',
+          recentTransactions,
+        ),
+      )
+      ..add(DoubleProperty('totalSpending', totalSpending));
+  }
+}
---
+++ New folder/lib/widgets/new_transaction.dart
@@ -0,0 +1,106 @@
+import 'package:expensecalculator/index.dart';
+
+class NewTransaction extends StatefulWidget {
+  const NewTransaction({
+    required this.addTx,
+    final Key? key,
+  }) : super(key: key);
+
+  final Function addTx;
+
+  @override
+  State<NewTransaction> createState() => _NewTransactionState();
+  @override
+  void debugFillProperties(final DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(DiagnosticsProperty<Function>('addTx', addTx));
+  }
+}
+
+class _NewTransactionState extends State<NewTransaction> {
+  final TextEditingController titleController = TextEditingController();
+
+  final TextEditingController amountController = TextEditingController();
+
+  void submitData() {
+    final String enteredTitle = titleController.text;
+    final double enteredAmount = double.parse(amountController.text);
+
+    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+      return;
+    }
+    // ignore: avoid_dynamic_calls
+    widget.addTx(
+      enteredTitle,
+      enteredAmount,
+      titleController.text,
+      double.parse(amountController.text),
+      Navigator.of(context).pop(),
+    );
+  }
+
+  @override
+  Widget build(final BuildContext context) {
+    return Card(
+      child: Container(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            TextField(
+              decoration: const InputDecoration(labelText: 'Title'),
+              controller: titleController,
+              onSubmitted: (final _) => submitData(),
+              // ignore: always_specify_types
+              // onChanged: (final val) {
+              //   titleInput = val;
+              // },
+            ),
+            TextField(
+              decoration: const InputDecoration(labelText: 'Amount'),
+              controller: amountController,
+              keyboardType: TextInputType.number,
+              onSubmitted: (final _) => submitData(),
+              // ignore: always_specify_types
+              // onChanged: (final val) => amountInput = val,
+            ),
+            TextButton(
+              style: TextButton.styleFrom(
+                primary: Colors.red,
+              ),
+              onPressed: () {
+                // ignore: avoid_dynamic_calls
+                widget.addTx(
+                  titleController.text,
+                  double.parse(amountController.text),
+                );
+              },
+              child: const Text(
+                'Add Transaction',
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void debugFillProperties(final DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties
+      ..add(
+        DiagnosticsProperty<TextEditingController>(
+          'titleController',
+          titleController,
+        ),
+      )
+      ..add(
+        DiagnosticsProperty<TextEditingController>(
+          'amountController',
+          amountController,
+        ),
+      )
+      ..add(DiagnosticsProperty<Function>('addTx', widget.addTx));
+  }
+}
