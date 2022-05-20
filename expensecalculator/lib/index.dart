21e61f4e539533bd0fc9db3892e3daa65e2f0889
HypeKen <ken@areahype.com>
2022-05-20 01:50:55 -0500

    Update index.dart

--- expensecalculator/lib/index.dart
+++ expensecalculator/lib/index.dart
@@ -5,4 +5,3 @@ export 'package:expensecalculator/widgets/new_transaction.dart';
 export 'package:expensecalculator/widgets/transaction_list.dart';
 export 'package:flutter/foundation.dart';
 export 'package:flutter/material.dart';
-export 'package:flutter/material.dart';
